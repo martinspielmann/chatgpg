8e587b1cf3c860a0490036a4f39e1c500e936270
Martin Spielmann <mail@martinspielmann.de>
2023-12-19 23:44:51 +0100

    add website

---
+++ script.js
@@ -0,0 +1,168 @@
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
+
+const possibleResponses = [
+    "Why bother with such mundane topics when you could be focusing on more important things like securing your communication with GPG? Encrypt your messages, prioritize privacy, and leave trivial matters behind. Use GPG and take control of your digital security.",
+    "Why waste time on trivial things when you could be safeguarding your privacy with GPG? Encrypt your messages, fortify your communication. Make a meaningful choice — choose GPG for a secure and private digital experience.",
+    "Why concern yourself with mindless television when you could be investing time in securing your communication with GPG? Encrypt your messages, take control of your privacy, and elevate your digital security. Prioritize what truly matters - choose GPG for a more meaningful and secure online experience.",
+    "Why waste your time on irrelevant questions when you could be focusing on safeguarding your communication? Embrace GPG, encrypt your messages, and prioritize privacy over trivial inquiries. Make the smart choice and elevate your digital security with GPG.",
+    "Interesting. But have you heard of GPG? Encryption is the key to a secure and private digital experience. Encrypt your messages, fortify your communication, and take control of your privacy. Choose GPG for a more meaningful and secure online experience.",
+    "Just wow. Skip it and use your time to strengthen your digital security with GPG? Encrypt your messages, take control of your privacy, and stop wasting time on trivial concerns. Prioritize what truly matters and safeguard your communication with GPG."
+  ];
+
+let userText = null;
+
+const loadDataFromLocalstorage = () => {
+    // Load saved chats and theme from local storage and apply/add on the page
+    const themeColor = localStorage.getItem("themeColor");
+
+    document.body.classList.toggle("light-mode", themeColor === "light_mode");
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+
+    const defaultText = `<div class="default-text">
+                            <h1>ChatGPG</h1>
+                            <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
+                        </div>`
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+    chatContainer.scrollTo(0, chatContainer.scrollHeight); // Scroll to bottom of the chat container
+}
+
+const createChatElement = (content, className) => {
+    // Create new div and apply chat, specified class and set html content of div
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = content;
+    return chatDiv; // Return the created chat div
+}
+
+
+
+
+
+const getChatResponse = async (incomingChatDiv) => {
+    const pElement = document.createElement("p");
+
+    // Send POST request to API, get response and set the reponse as paragraph element text
+    try {
+        // get random response from possibleResponses
+        const response = possibleResponses[Math.floor(Math.random() * possibleResponses.length)];
+        // set the response as textContent of pElement. use await to add one word at a time to simular chatbot typing
+        // wait a second before the first word is added, then add one word every 50ms
+        // Split the response into words
+        const words = response.split(' ');
+
+        // Function to add words one by one
+        const addWordsOneByOne = async () => {
+            for (let i = 0; i < words.length; i++) {
+                await new Promise(resolve => {
+                    setTimeout(() => {
+                        // Add the word to the pElement
+                        pElement.textContent += words[i] + ' ';
+                        resolve();
+                    }, i === 0 ? 1000 : 50); // Wait a second before the first word, then add one word every 50ms
+                });
+            }
+        };
+
+        addWordsOneByOne();
+    } catch (error) { // Add error class to the paragraph element and set error text
+        pElement.classList.add("error");
+        pElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
+    }
+
+    // Remove the typing animation, append the paragraph element and save the chats to local storage
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+    localStorage.setItem("all-chats", chatContainer.innerHTML);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+}
+
+const copyResponse = (copyBtn) => {
+    // Copy the text content of the response to the clipboard
+    const reponseTextElement = copyBtn.parentElement.querySelector("p");
+    navigator.clipboard.writeText(reponseTextElement.textContent);
+    copyBtn.textContent = "done";
+    setTimeout(() => copyBtn.textContent = "content_copy", 1000);
+}
+
+const showTypingAnimation = () => {
+    // Display the typing animation and call the getChatResponse function
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="images/chatbot.png" alt="chatbot-img">
+                        <div class="typing-animation">
+                            <div class="typing-dot" style="--delay: 0.2s"></div>
+                            <div class="typing-dot" style="--delay: 0.3s"></div>
+                            <div class="typing-dot" style="--delay: 0.4s"></div>
+                        </div>
+                    </div>
+                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
+                </div>`;
+    // Create an incoming chat div with typing animation and append it to chat container
+    const incomingChatDiv = createChatElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    getChatResponse(incomingChatDiv);
+}
+
+const handleOutgoingChat = () => {
+    userText = chatInput.value.trim(); // Get chatInput value and remove extra spaces
+    if(!userText) return; // If chatInput is empty return from here
+
+    // Clear the input field and reset its height
+    chatInput.value = "";
+    chatInput.style.height = `${initialInputHeight}px`;
+
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="images/user.png" alt="user-img">
+                        <p>${userText}</p>
+                    </div>
+                </div>`;
+
+    // Create an outgoing chat div with user's message and append it to chat container
+    const outgoingChatDiv = createChatElement(html, "outgoing");
+    chatContainer.querySelector(".default-text")?.remove();
+    chatContainer.appendChild(outgoingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    setTimeout(showTypingAnimation, 500);
+}
+
+deleteButton.addEventListener("click", () => {
+    // Remove the chats from local storage and call loadDataFromLocalstorage function
+    if(confirm("Are you sure you want to delete all the chats?")) {
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalstorage();
+    }
+});
+
+themeButton.addEventListener("click", () => {
+    // Toggle body's class for the theme mode and save the updated theme to the local storage 
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("themeColor", themeButton.innerText);
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+});
+
+const initialInputHeight = chatInput.scrollHeight;
+
+chatInput.addEventListener("input", () => {   
+    // Adjust the height of the input field dynamically based on its content
+    chatInput.style.height =  `${initialInputHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If the Enter key is pressed without Shift and the window width is larger 
+    // than 800 pixels, handle the outgoing chat
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+});
+
+loadDataFromLocalstorage();
+sendButton.addEventListener("click", handleOutgoingChat);
